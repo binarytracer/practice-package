1fda0b90f64eadd06af5a7702005638d843658a3
Rod <gohetiaroderick@gmail.com>
2025-05-23 01:21:13 +0800

    chore: quick import test

--- src/schemas/common/index.ts
+++
@@ -1,1 +0,0 @@
-export { uuidSchema } from "./uuid-schema";
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,1 +1,7 @@
-export { uuidSchema } from "./common/uuid-schema";
+import { Type } from "@sinclair/typebox";
+
+export const uuidSchema = Type.String({
+  pattern:
+    "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
+  description: "UUID v4",
+});
